6ac16ca8116b89380e03f9274f61f9857f221873
charles-nasd <cejiegbu@nasdng.com>
2023-04-27 12:41:47 +0100

    feat: added new users to seed

--- src/data/userData.ts
+++ src/data/userData.ts
@@ -2,6 +2,12 @@ import { ROLES } from "../enum/roles";
 
 const USERS = [
 	{
+		name: "Chinwendu Ekeh",
+		email: "super_admin@gmail.com",
+		password: "password",
+		role: ROLES.super_admin,
+	},
+	{
 		name: "Shaun Murphy",
 		email: "admin@gmail.com",
 		password: "password",
@@ -30,6 +36,12 @@ const USERS = [
 		email: "dda@gmail.com",
 		password: "password",
 		role: ROLES.dda,
+	},
+	{
+		name: "Mercy Johnson",
+		email: "trustee@gmail.com",
+		password: "password",
+		role: ROLES.trustee,
 	},
 ];
 
--- src/enum/roles.ts
+++ src/enum/roles.ts
@@ -1,7 +1,9 @@
 export enum ROLES {
+	super_admin = "super_admin",
 	admin = "admin",
 	issuer = "issuer",
 	investor = "investor",
 	broker = "broker",
 	dda = "dda",
+	trustee = "trustee",
 }
